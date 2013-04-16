89e387b2a10c4511259ad9f6f3ccd93137a72494
刘璇 <liuxuan@liuxuantekiMacBook-Air.local>
2013-04-16 16:57:18 +0800

    Initial Commit

---
+++ TestCellFitHeightAuto/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestCellFitHeightAuto
+//
+//  Created by 刘璇 on 13-4-16.
+//  Copyright (c) 2013年 刘璇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestCellFitHeightAuto/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestCellFitHeightAuto
+//
+//  Created by 刘璇 on 13-4-16.
+//  Copyright (c) 2013年 刘璇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
